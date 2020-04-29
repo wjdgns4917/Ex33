93216c8a4b719d6b5672e615807c998647fd84bb
wjdgns4917 <hoony0802@naver.com>
2020-04-29 09:14:27 +0900

    Ex33

---
+++ src/ex/InterfaceEx.java
@@ -0,0 +1,33 @@
+package ex;
+
+interface PhoneInterface{ //인터페이스 선언
+	final int TIMEOUT=10000; //상수 필드 선언
+	void sendCall(); //추상 메소드
+	void receiveCall(); //추상 메소드
+	default void printLogo() { //default 메소드
+		System.out.println("** Phone **");
+	}
+}
+class SansungPhone implements PhoneInterface{ //인터페이스 구현
+	//PhoneInterface의 모든 추상 메소드 구현
+	@Override
+	public void sendCall() {
+		System.out.println("띠리링");
+	}
+
+	@Override
+	public void receiveCall() {
+		System.out.println("전화가 왔습니다.");		
+	} 
+	//메소드 추가 작성
+	public void flash() {System.out.println("전화기에 불이 켜졌습니다.");}
+}
+public class InterfaceEx {
+	public static void main(String[] args) {
+		SansungPhone phone=new SansungPhone();
+		phone.printLogo();
+		phone.sendCall();
+		phone.receiveCall();
+		phone.flash();
+	}
+}
